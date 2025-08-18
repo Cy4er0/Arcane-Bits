9d43e2de7daa2bdc1f5ed557786b9ffa9e47ba32
cy4er0 <214340788+Cy4er0@users.noreply.github.com>
2025-08-18 16:27:08 +0100

    I got the wrong files pushed
      pushed the correct files
    	modified:   index.html
    	modified:   script.js
    	modified:   style.css

--- script.js
+++ script.js
@@ -1,68 +1,56 @@
-let xp = 0;
-let health = 100;
-let gold = 50;
-let currentWeapon = 0;
-let fighting; 
-let monsterHealth;
-let inventory = ["stick"];
+const btc1= document.getElementsByClassName("btn1");
+const btn2=document.getElementsByClassName("btn2");
+const btn3=document.getElementsByClassName("btn3");
+const text=document.getElementById("text");
 
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const trapStats = document.querySelector("#trapStats");
-const trapName = document.querySelector("#trapName");
-//const monsterHealthText = document.querySelector("#monsterHealth");
-
-//change monsters with traps
-  const traps=[
-    {
-    name:'broken glass shards',
-    "button text":["return back","defend"],
-    "button functions":[returnBack,defend],
-    text:"You stepped on glass shards you receive 5 damage"
-
+const actions=[{
+    name: "go right",
+    "button text":["return back","go to the store","use the potion"],
+    "button functions":[returnBack,goStore,usePotion],
+    text: " you fell into a trap, your health is reduced"
 
 },
 {
-    name: "sharp arrows",
-    "button text":["returnBack","dodge"],
-    "button functions":{returnBack,dodge},
-    text:"You have been hit by an arrow you receive 10 damage"
-
+    name: "go left",
+    "button text":["go left","go to the store","go right"],
+    "button functions":[goLeft,goStore,goRight],
+    text:"you found the first piece"
 },
 {
-    name: "throwing knives",
-    "button text":["returnBack","dodge"],
-    "button functions":[returnBack,dodge],
-    text:"you got hit by a knife you receive 20 damage"
+    name: "go to store",
+    "button text":["return back","buy health","buy the potion"],
+    "button functions":[returnBack,buyHealth,buyPotion],
+    text:"you entered the store , choose what you want to buy"
 }
-]
+];
 
-const locations =[{
-  name:"square",
-  "button text" :["Go left","Go to store","Go right"],
-  "button functions":[goLeft,goStore,goRight],
-  text:"You awaken in the ruins with no mwmory drawn toward a glowing fragment.As touch it whispers flood your mind a deep voice awaken from the cold piece "
+const update=(action)=>{
+    btn1.innerText=action["button text"][0];
+    btn2.innerText=action["button text"][1];
+    btn3.innerText=action["button text"][2];
+    btn1.onclick=action["button functions"][0];
+    btn2.onclick=action["button functions"][1];
+    btn3.onclick=action["button functions"][2];
+    text.innerHTML=action.text;
 
-},
-{
-  name: "store",
-  "button text":["Buy 20 health(10gold)","Buy protective rune","Go to the square"],
-  "button funtions":[buyHealth, buyRune, goSquare],
-  text:"You eter store"
-},
-{
-  name:"Right",
-  "button text":["return to the previous room","Go to the store","Use the protective rune"],
-  "button functions":[goBack,goStore,useRune],
-  text:" You enter the right door"
-},
-{
-  name:"left",
-  "button text":{"return to the previous room","Go to the store","Use the protective rune"},
-  "button functions":
-}]
+}
+
+const goLeft=()=>{
+    update(actions[0]);
+}
+
+const goStore=()=>{
+    update(actions[1]);
+}
+
+const goRight=()=>{
+    update(actions[2]);
+}
+
+btn1.onclick=goLeft();
+btn2.onclick=goStore();
+btn3.onclick=goRight();
+
+btn1.addEventListener("click",goLeft);
+btn2.addEventListener("click",goStore);
+btn3.addEventListener("click",goRight);
